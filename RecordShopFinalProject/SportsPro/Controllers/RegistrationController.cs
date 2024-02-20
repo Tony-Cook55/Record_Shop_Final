a1c71a7211b052c864182e129969c9fb8718e540
Tony Cook <Anthony_Cook@insideranken.org>
2024-02-20 09:52:38 -0600

    added active link for about page

---
+++ RecordShopFinalProject/SportsPro/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecordShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
