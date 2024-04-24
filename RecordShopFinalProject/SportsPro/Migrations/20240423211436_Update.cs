e38e57061bdeb060cfe9d8f83b040d6624753664
Tony-Cook55 <nogdog55@gmail.com>
2024-04-23 21:22:29 -0500

    Login Page Css Done (I Think)

---
+++ RecordShopFinalProject/SportsPro/Migrations/20240423211436_Update.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentModelId",
+                keyValue: 1,
+                column: "DateOpened",
+                value: new DateTime(2024, 4, 23, 16, 14, 36, 347, DateTimeKind.Local).AddTicks(589));
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentModelId",
+                keyValue: 2,
+                column: "DateOpened",
+                value: new DateTime(2024, 4, 23, 16, 14, 36, 347, DateTimeKind.Local).AddTicks(594));
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentModelId",
+                keyValue: 3,
+                column: "DateOpened",
+                value: new DateTime(2024, 4, 23, 16, 14, 36, 347, DateTimeKind.Local).AddTicks(598));
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentModelId",
+                keyValue: 4,
+                column: "DateOpened",
+                value: new DateTime(2024, 4, 23, 16, 14, 36, 347, DateTimeKind.Local).AddTicks(602));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentModelId",
+                keyValue: 1,
+                column: "DateOpened",
+                value: new DateTime(2024, 4, 19, 11, 19, 18, 223, DateTimeKind.Local).AddTicks(1019));
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentModelId",
+                keyValue: 2,
+                column: "DateOpened",
+                value: new DateTime(2024, 4, 19, 11, 19, 18, 223, DateTimeKind.Local).AddTicks(1095));
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentModelId",
+                keyValue: 3,
+                column: "DateOpened",
+                value: new DateTime(2024, 4, 19, 11, 19, 18, 223, DateTimeKind.Local).AddTicks(1148));
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentModelId",
+                keyValue: 4,
+                column: "DateOpened",
+                value: new DateTime(2024, 4, 19, 11, 19, 18, 223, DateTimeKind.Local).AddTicks(1153));
+        }
+    }
+}
